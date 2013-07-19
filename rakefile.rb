d55f4c203d107844a61d26ba269d3eb9dd5a5888
Rick <rick@cloudscaling.com>
2013-07-19 00:59:49 -0700

    added github for gem homepage

--- rakefile.rb
+++ rakefile.rb
@@ -6,6 +6,7 @@ Buildar.new do |b|
   b.version_file = 'VERSION'
   b.gemspec.name = 'dotcfg'
   b.gemspec.summary = 'simple filesystem de/serialization for app configs'
+  b.gemspec.homepage = 'https://github.com/rickhull/dotcfg'
   b.gemspec.author = 'Rick Hull'
   b.gemspec.license = 'MIT'
   b.gemspec.description = 'JSON and YAML config serialization and synch'
