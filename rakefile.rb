18a6fd5549261ba16ed884c3b193506d783f2756
Rick <rick@cloudscaling.com>
2013-07-19 17:35:20 -0700

    added json dep

--- rakefile.rb
+++ rakefile.rb
@@ -9,6 +9,7 @@ Buildar.new do |b|
   b.gemspec.homepage = 'https://github.com/rickhull/dotcfg'
   b.gemspec.author = 'Rick Hull'
   b.gemspec.license = 'MIT'
-  b.gemspec.description = 'JSON and YAML config serialization and synch'
+  b.gemspec.description = 'JSON and YAML config serialization and persistence'
   b.gemspec.files = %w{rakefile.rb lib/dotcfg.rb}
+  b.gemspec.add_runtime_dependency 'json', '~> 1'
 end
