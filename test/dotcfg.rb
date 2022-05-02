1f58fd9638ea03b213e3d4c620f79362c8aa3a1e
Rick Hull <rick.hull@gmail.com>
2022-05-02 07:55:50 -0400

    use expect() wrapper for MiniTest 6

--- test/dotcfg.rb
+++ test/dotcfg.rb
@@ -15,17 +15,12 @@ describe DotCfg do
     @dc = DotCfg.new(@filename)
   end
 
-  describe "new" do
-    describe "bad file path" do
-      it "must raise" do
-        proc { DotCfg.new('does/not/exist') }.must_raise Errno::ENOENT
-      end
+  describe "bad initialization" do
+    it "raises" do
+      expect { DotCfg.new('does/not/exist') }.must_raise Errno::ENOENT
     end
-
-    describe "nonexistent valid file path" do
-      it "must create a new file" do
-        File.exist?(@filename).must_equal true
-      end
+    it "creates a new file" do
+      expect(File.exist?(@filename)).must_equal true
     end
   end
 
@@ -36,23 +31,23 @@ describe DotCfg do
       end
 
       describe "nonexistent keys" do
-        it "must return nil" do
-          @dc[:does_not_exist].must_be_nil
+        it "returns nil" do
+          expect(@dc[:does_not_exist]).must_be_nil
         end
       end
 
       describe "existing keys" do
-        it "must return the value" do
-          @dc['hello'].must_equal 'world'
+        it "returns the value" do
+          expect(@dc['hello']).must_equal 'world'
         end
       end
 
       describe "key removal" do
-        it "must return nil" do
+        it "returns nil" do
           @dc[:hello] = :world
-          @dc[:hello].must_equal :world
+          expect(@dc[:hello]).must_equal :world
           @dc.delete :hello
-          @dc[:hello].must_be_nil
+          expect(@dc[:hello]).must_be_nil
         end
       end
 
@@ -69,26 +64,26 @@ describe DotCfg do
     end
 
     it "must serialize to a string" do
-      @dc.serialize.must_be_kind_of String
+      expect(@dc.serialize).must_be_kind_of String
     end
 
     it "must deserialize its serialization" do
       hsh = @dc.deserialize(@dc.serialize)
-      hsh.must_be_kind_of Hash
-      hsh.must_equal({ 'hello' => 'world', 'goodbye' => 'cruel world' })
-      @dc['hello'].must_equal 'world'
-      @dc['goodbye'].must_equal 'cruel world'
+      expect(hsh).must_be_kind_of Hash
+      expect(hsh).must_equal({ 'hello' => 'world', 'goodbye' => 'cruel world' })
+      expect(@dc['hello']).must_equal 'world'
+      expect(@dc['goodbye']).must_equal 'cruel world'
     end
 
     it "must overwrite when deserializing" do
       str = @dc.serialize
       @dc.delete 'hello'
       @dc.deserialize(str)
-      @dc['hello'].must_equal 'world'
+      expect(@dc['hello']).must_equal 'world'
     end
 
     it "must pretty-serialize to a string" do
-      @dc.pretty.must_be_kind_of String
+      expect(@dc.pretty).must_be_kind_of String
     end
 
     # eh, needed?
